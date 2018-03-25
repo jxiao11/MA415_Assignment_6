e5519c20f5b60d306f525df0112c46cb02692b7e
Junpei Xiao <foreverjunpei@yahoo.com>
2018-03-25 12:19:43 +0800

    Add hotels by Junpei Xiao

--- Assignment_6_Bude_code.R
+++ Assignment_6_Bude_code.R
@@ -66,3 +66,56 @@ ggmap(m_w) +
         axis.title.y=element_blank(),legend.position="none")
 #geom_text(x=-4.545, y=50.825, label="Bude",fontface =2,size=10, colour = "tomato4")
 
+
+
+
+###--------------------------------------
+### Add hotels by Junpei Xiao
+###--------------------------------------
+
+### Create a dataframe for all gecode. The gecode code function may cause over limit query issue
+### prefer to type in those geocode.
+### Crooklets Beach Cafe: -4.553029,50.83556
+### Edgecumbe Hotel:-4.547583 50.83138
+
+df_lon <- c(-4.553962,-4.551349,-4.552814,-4.543023,-4.547858,-4.547583)
+df_lat <- c(50.83587,50.83054,50.83347,50.83007,50.82697,50.83138)
+df4 <- data.frame("lon" = df_lon, "lat" = df_lat)
+Location <- c("Crooklet's Beach","Summerleaze Beach","Cricket ground","The Barrel Pub","Falcon hotel","Edgecumbe Hotel")
+df5 <- cbind(df4,Location)
+
+rou <- route(source, dest,mode="walking", structure = "route")
+
+#### get Route_map
+route_map <- get_googlemap(center = c(lon=-4.543678,lat=50.82664),zoom = 14)
+
+ggmap(route_map) +
+  geom_path(
+    aes(x = lon, y = lat), colour = "#ff9f84", size = 1,
+    data = rou, lineend = "round"
+  ) +
+  geom_point(
+    aes(x = lon, y = lat, col = Location),
+    data = df5,size = 4) +
+  theme(axis.line=element_blank(),axis.text.x=element_blank(),
+        axis.text.y=element_blank(),axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank())
+
+### get water map
+water_map <- get_map(df4,source="stamen",zoom = 15,maptype = "watercolor")
+
+ggmap(water_map) +
+  geom_label(
+    aes(x = lon, y = lat, label = Location, col=Location,size=3),
+    fontface = "bold" ,data = df5,
+    vjust = 1.3, hjust = (0.5)
+  ) +
+  geom_point(
+    aes(x = lon, y = lat, col = Location),
+    data = df5, size = 3 ) +
+  theme(axis.line=element_blank(),axis.text.x=element_blank(),
+        axis.text.y=element_blank(),axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),legend.position="none")
+
